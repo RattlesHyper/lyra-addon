3d1b61a989fd8bf9d430cc42b1049f7847c54a13
xlzxq <104225234+xlzxq@users.noreply.github.com>
2023-08-30 09:06:55 +0100

    prevent another crash (possibly)

--- src/main/java/com/lyra/addon/commands/StickCommand.java
+++ src/main/java/com/lyra/addon/commands/StickCommand.java
@@ -46,7 +46,7 @@ public class StickCommand extends Command {
             keepStuck = true;
             new Thread(() -> {
                 for (int i = 1; i > 0; i++) {
-                    if (mc.player != null && target != null && keepStuck)
+                    if (mc.player != null && target != null && mc.player.distanceTo(target) < 6 && keepStuck)
                         mc.player.setPosition(target.getX(), target.getY() + 2, target.getZ());
                 }
 			}).start();
